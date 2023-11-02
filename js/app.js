e5fc4a6b7bd37954a4f0d6f2d80310881b803b86
mariasolegallina <gallinamariasole@gmail.com>
2023-11-02 16:11:55 +0100

    add - one random email

--- js/app.js
+++ js/app.js
@@ -1,11 +1,35 @@
 const { createApp } = Vue
 
-console.log(axios)
+// console.log(axios)
 
 createApp({
     data() {
         return {
-            message: 'itopinonavevanonipoti'
+            message: 'itopinonavevanonipoti',
+            email:'',
         }
     },
+    methods: {
+        fetchEmail() {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((responseEmail) => {
+                        const randomEmail = responseEmail.data.response
+                        this.email = randomEmail
+                        console.log('questa è la mail random', this.email)
+                    }
+                )
+        },
+
+    },
+    created() {
+        this.fetchEmail()
+    }
+
 }).mount('#app')
+
+axios
+    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((resp) => {
+        console.log(resp.data.response)
+    })
