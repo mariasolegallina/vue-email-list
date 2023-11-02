17c3bdb17dbf38886f04a1e202074ba3b9415d79
mariasolegallina <gallinamariasole@gmail.com>
2023-11-02 17:29:36 +0100

    add - 10 random emails in DOM

--- js/app.js
+++ js/app.js
@@ -1,35 +1,34 @@
 const { createApp } = Vue
 
-// console.log(axios)
-
 createApp({
     data() {
         return {
             message: 'itopinonavevanonipoti',
-            email:'',
+            email: '',
+            arrayEmails: [],
         }
     },
     methods: {
         fetchEmail() {
-            axios
+
+            // chiedere al server di generare un'email random per 10 volte
+            for (let i = 0; i < 10; i++) {
+
+                // chiedere al server di generare un'email random
+                axios
                 .get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((responseEmail) => {
-                        const randomEmail = responseEmail.data.response
-                        this.email = randomEmail
-                        console.log('questa è la mail random', this.email)
-                    }
-                )
-        },
+                    const randomEmail = responseEmail.data.response
+                    this.email = randomEmail
+                    console.log('questa è la mail random', this.email)
 
+                    // push dell'indirizzo email nell'array
+                    this.arrayEmails.push(randomEmail)
+                })
+            }
+        },
     },
     created() {
         this.fetchEmail()
     }
-
 }).mount('#app')
-
-axios
-    .get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((resp) => {
-        console.log(resp.data.response)
-    })
